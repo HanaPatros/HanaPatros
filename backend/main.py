d015539405ddb393d9c1c29d9fca467961f02bb2
hanapatros@hotmail.com <hanapatros@hotmail.com>
2022-08-11 13:43:40 +0200

    add remaining files for db connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title="Jobboard", varion="0.1.0")
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,
+                  version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = FastAPI(title="Jobboard", version="0.1.0")
+
+
+app = start_application()
+
 
 @app.get("/")
 def hello_api():
