dddb00bb848398053aff42a1e1e8572511bd8d57
hanapatros@hotmail.com <hanapatros@hotmail.com>
2022-08-14 17:11:41 +0200

    Hashing

--- backend/core/hashing.py
+++ backend/core/hashing.py
@@ -1,8 +1,8 @@
 from passlib.context import CryptContext
 
-pwt_context = CryptContext(Schemes=["bcrypt"], deprecated="auto")
+pwt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class Hasher():
+class Hasher:
     @staticmethod
     def verify_password(plain_password, hashed_password):
         return pwt_context.verify(plain_password, hashed_password)
